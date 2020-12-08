77191b7b053037e22fefe8d7d725f63dc7c69c33
arvindrenga96 <arvind.renga96@gmail.com>
2020-12-08 13:57:17 -0600

    Changing install.sh and YAML defaults

--- full_install.sh
+++ full_install.sh
@@ -1,7 +1,13 @@
-pip install -e core/
-pip install -e tabular/
-pip install -e mxnet/
-pip install -e extra/
-pip install -e text/
-pip install -e vision/
-pip install -e autogluon/
+sudo apt-get update
+sudo apt install python3-pip
+pip3 instaLL torch
+pip3 install torchtext
+pip3 install torchvision
+pip3 install mxnet
+pip3 install -e core/
+pip3 install -e tabular/
+pip3 install -e mxnet/
+pip3 install -e extra/
+pip3 install -e text/
+pip3 install -e vision/
+pip3 install -e autogluon/
