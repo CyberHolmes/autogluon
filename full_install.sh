7c7a65b96bd1ebf6ffe9b6ff64db94c1adeede62
arvindrenga96 <arvind.renga96@gmail.com>
2020-12-08 17:06:14 -0600

    fix full_install.sh

--- full_install.sh
+++ full_install.sh
@@ -1,18 +1,6 @@
 sudo apt-get update
+sudo apt-get install gcc python3-dev
 curl -sL \
   "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" > \
   "Miniconda3.sh"
 bash Miniconda3.sh
-sudo apt-get install gcc python3-dev
-pip install torch
-pip install torchvision
-pip install torchtext
-pip install mxnet
-pip install -e core/
-pip install -e tabular/
-pip install -e mxnet/
-pip install -e extra/
-pip install -e text/
-pip install -e vision/
-pip install -e autogluon/
-mkdir -p benchmarking/output/jct
---
+++ full_install2.sh
@@ -0,0 +1,12 @@
+pip install torch
+pip install torchvision
+pip install torchtext
+pip install mxnet
+pip install -e core/
+pip install -e tabular/
+pip install -e mxnet/
+pip install -e extra/
+pip install -e text/
+pip install -e vision/
+pip install -e autogluon/
+mkdir -p benchmarking/output/jct
