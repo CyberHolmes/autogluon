f6cfd6bebf80d05cc00f271ef70f7ca48f2c9bf9
prashanth <prashanth.flash98@gmail.com>
2020-12-10 12:26:10 -0600

    changed number of search spaces

--- benchmarking/rl_benchmark.py
+++ benchmarking/rl_benchmark.py
@@ -484,7 +484,7 @@ if __name__ == "__main__":
     # run the experiment multiple time
     for experiment in range(args.bootstrap):
         # run the experiment for different search space sizes
-        for space_size in range(2, 10):
+        for space_size in [2, 4, 8]:
             # create all the tasks
             tasks, search_spaces = create_tasks(args.conf)
 
