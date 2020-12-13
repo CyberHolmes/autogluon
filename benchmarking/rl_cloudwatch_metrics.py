6f160eeee5a7848fda8920f2b8378c6e6afdb65d
arvindrenga96 <arvind.renga96@gmail.com>
2020-12-13 11:19:13 -0600

    Changed cloudwatchmetric code to get gpu metrics as well

--- benchmarking/rl_cloudwatch_metrics.py
+++ benchmarking/rl_cloudwatch_metrics.py
@@ -10,7 +10,7 @@ import argparse
 #scp -i "autogluon_arvind.pem" -r ubuntu@ec2-54-157-243-83.compute-1.amazonaws.com:~/autogluon/benchmarking/output/jct/ ./autog
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--out', default='output/resource_util', help='output file')
+parser.add_argument('--out', default='output/resource_util/ag1_1', help='output file')
 
 args = parser.parse_args()
 if not os.path.exists(args.out):
@@ -23,7 +23,7 @@ jctlog = pd.read_csv("./output/jct/rl_benchmark.csv")
 cloudwatch = boto3.client('cloudwatch')
 
 # create the results dataframe
-results = pd.DataFrame({
+results_cpu = pd.DataFrame({
     'task': [],
     'controller': [],
     'sync': [],
@@ -34,9 +34,24 @@ results = pd.DataFrame({
     'search_space_size': [],
     'machine': [],
     'timestamp': [],
-    'resource_utilization':[]
+    'cpu_utilization':[]
 })
-EC2_instances = ['i-0742c2217fad05918', 'i-0c67e0d0d0c89cca2']
+results_gpu = pd.DataFrame({
+    'task': [],
+    'controller': [],
+    'sync': [],
+    'runtime': [],
+    'accuracy': [],
+    'num_machines': [],
+    'experiment': [],
+    'search_space_size': [],
+    'machine': [],
+    'timestamp': [],
+    'gpu_utilization':[]
+})
+EC2_instances = ['i-0cb9872251019a481']
+#EC2_instances = ['i-0750fccf339cbdf4f', 'i-07346a205ec87da05']
+#EC2_instances = ['i-0cb9872251019a481',  'i-0750fccf339cbdf4f', 'i-07346a205ec87da05']
 
 for i,ec2 in enumerate(EC2_instances):
     for j in range(jctlog.shape[0]):
@@ -60,9 +75,25 @@ for i,ec2 in enumerate(EC2_instances):
                 'Average'
             ]
         )
+        response2 = cloudwatch.get_metric_statistics(
+            Namespace='DeepLearningTrain',
+            Dimensions=[
+                {
+                    'Name': 'InstanceId',
+                    'Value': ec2
+                },
+            ],
+            MetricName='GPU Usage',
+            StartTime=starttime,
+            EndTime=endtime,
+            Period=60,
+            Statistics=[
+                'Average'
+            ]
+        )
         Datapoints1 = response['Datapoints']
         for datapoint in Datapoints1:
-            results = results.append({
+            results_cpu = results_cpu.append({
                 'task': row['task'].iloc[0],
                 'controller': row['controller'].iloc[0],
                 'sync': row['sync'].iloc[0],
@@ -73,7 +104,23 @@ for i,ec2 in enumerate(EC2_instances):
                 'search_space_size': row['search_space_size'].iloc[0],
                 'machine': i,
                 'timestamp': datapoint['Timestamp'],
-                'resource_utilization': datapoint['Average']
+                'cpu_utilization': datapoint['Average']
+            }, ignore_index=True)
+        Datapoints2 = response2['Datapoints']
+        for datapoint in Datapoints2:
+            results_gpu = results_gpu.append({
+                'task': row['task'].iloc[0],
+                'controller': row['controller'].iloc[0],
+                'sync': row['sync'].iloc[0],
+                'runtime': row['runtime'].iloc[0],
+                'accuracy': row['accuracy'].iloc[0],
+                'num_machines': row['num_machines'].iloc[0],
+                'experiment': row['experiment'].iloc[0],
+                'search_space_size': row['search_space_size'].iloc[0],
+                'machine': i,
+                'timestamp': datapoint['Timestamp'],
+                'cpu_utilization': datapoint['Average']
             }, ignore_index=True)
             # save the experiment details
-            results.to_csv(args.out+'/'+'rl_benchmark.csv')
+results_cpu.to_csv(args.out+'/'+'rl_cpu_util.csv')
+results_gpu.to_csv(args.out+'/'+'rl_gpu_util.csv')
